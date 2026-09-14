ea3f46177660827ee3a44415d6d245518c560773
ArielJoshuaLumbera <arieljoshua847@gmail.com>
2026-09-14 22:48:58 +0800

    Added code to log in

--- app/src/test/java/com/example/studyguard/Minipeta3/Login.java
+++ app/src/test/java/com/example/studyguard/Minipeta3/Login.java
@@ -1,4 +1,22 @@
 package com.example.studyguard.Minipeta3;
 
+
 public class Login {
+
+        public static void main(String[] args) {
+
+            String username = "student";
+            String password = "12345";
+
+            System.out.println("===== STUDY GUARD LOGIN =====");
+            System.out.println("Username: " + username);
+            System.out.println("Password: " + password);
+
+            if (username.equals("student") && password.equals("12345")) {
+                System.out.println("Login Successful!");
+            } else {
+                System.out.println("Invalid Username or Password!");
+            }
+        }
+    }
 }
