ba6e89c44af5f75b603ac9002afa0826dfad3cbf
peter-batario-ict <peter.batario1@gmail.com>
2026-09-14 14:15:38 +0800

    change pet name

--- app/src/test/java/com/example/studyguard/Quarter2/Marquez_MiniPeta1.java
+++ app/src/test/java/com/example/studyguard/Quarter2/Marquez_MiniPeta1.java
@@ -8,7 +8,7 @@ public class Marquez_MiniPeta1 {
     public void printMyProfile() {
         // --- 1. THE INPUT (Storing your personal details in variables) ---
         String myName = "Gab";
-        String petName = "Snowy";
+        String petName = "Bucky";
         String favFood = "Chicken";
         int myAge = 17;
 
