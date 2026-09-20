cc70f3ace2c64c173e4e4ce7e830b8f9a2becea7
PrinzGianFlores <ikawaydilaw25@gmail.com>
2026-09-21 01:40:28 +0800

    Logout Commit

--- app/src/test/java/com/example/studyguard/Minipeta3/Logout.java
+++ app/src/test/java/com/example/studyguard/Minipeta3/Logout.java
@@ -1,1 +1,33 @@
+package com.example.studyguard.Minipeta3;
 
+import java.time.LocalDateTime;
+
+public class Logout {
+
+    private String username;
+    private boolean loggedIn;
+    private LocalDateTime logoutTime;
+
+    public Logout(String username) {
+        this.username = username;
+        this.loggedIn = true;
+    }
+
+    public void logout() {
+        loggedIn = false;
+        logoutTime = LocalDateTime.now();
+
+        System.out.println(username + " has logged out.");
+        System.out.println("Logout time: " + logoutTime);
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+    public static void main(String[] args) {
+        Logout session = new Logout("student01");
+
+        session.logout();
+    }
+}
