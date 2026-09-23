1315c5c9ceb11b07a3d61861358299c59fe6814c
Gab Marquez <2014-10149@mcstayuman.edu.ph>
2026-09-23 23:25:03 +0800

    Menu loop added

--- app/src/test/java/quarter2/practicalexam/GymAccess.java
+++ app/src/test/java/quarter2/practicalexam/GymAccess.java
@@ -6,8 +6,25 @@ import java.util.Scanner;
 
 public class GymAccessTest {
 
+    public void start(Scanner scanner) {
+
+        boolean running = true;
+
+        while (running) {
+
+            System.out.println("===== GYM ACCESS SYSTEM =====");
+            System.out.println("1. Enter Gym");
+            System.out.println("2. Hire Trainer");
+            System.out.println("3. Exit");
+
+
+        }
+    }
+
     @Test
     public void testGymFlow() {
+
+
 
     }
 }
